3855c6a39718a49b815651d5140af5c50e189396
Elly <ellychau10@yahoo.com>
2021-05-18 13:56:53 -0700

    madLib.js

---
+++ madLib.js
@@ -0,0 +1,6 @@
+
+const madLib = (verb, adj, noun) => {
+    return console.log('we shall ' + verb.toUpperCase() + ' the ' + adj.toUpperCase() + ' ' + noun.toUpperCase() );
+}
+
+madLib('make','best', 'guac');
